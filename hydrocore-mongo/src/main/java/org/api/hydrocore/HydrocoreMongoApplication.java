a219c1c8dbbb5d8fca902b5983366c995057a16a
clarabartolini <clarinhabartolini321@gmail.com>
2025-10-06 10:36:39 -0300

    Inicial commit

---
+++ hydrocore-mongo/src/main/java/org/api/hydrocore/HydrocoreMongoApplication.java
@@ -0,0 +1,13 @@
+package org.api.hydrocore;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HydrocoreMongoApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HydrocoreMongoApplication.class, args);
+    }
+
+}
---
+++ hydrocore-mongo/src/test/java/org/api/hydrocore/HydrocoreMongoApplicationTests.java
@@ -0,0 +1,13 @@
+package org.api.hydrocore;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class HydrocoreMongoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
