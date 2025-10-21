5ac5e92836e37dc1518a7704a9d8f34311178a7e
leonardolinsz <leonardo.furlanis@germinare.org.br>
2025-10-21 08:43:01 -0300

    Arrumando email

--- src/main/java/org/api/hydrocore/service/EmailService.java
+++ src/main/java/org/api/hydrocore/service/EmailService.java
@@ -1,30 +1,38 @@
 package org.api.hydrocore.service;
 
-import lombok.RequiredArgsConstructor;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.mail.SimpleMailMessage;
-import org.springframework.mail.javamail.JavaMailSender;
+import okhttp3.*;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
+import java.io.IOException;
 
 @Service
 public class EmailService {
 
-    private final JavaMailSender mailSender;
-
-    @Autowired
-    public EmailService(JavaMailSender mailSender) {
-        this.mailSender = mailSender;
-    }
+    @Value("${MAILERSEND_API_KEY}")
+    private String apiKey;
 
     public void sendResetPasswordEmail(String to, String token) {
         String link = "intervireya://reset-password?token=" + token;
+        String subject = "Redefinição de senha - HydroCore";
+        String text = "Olá!\n\nClique no link abaixo para redefinir sua senha:\n" +
+                link + "\n\nEste link expira em 15 minutos.";
 
-        SimpleMailMessage message = new SimpleMailMessage();
-        message.setTo(to);
-        message.setSubject("Redefinição de senha - HydroCore");
-        message.setText("Olá!\n\nClique no link abaixo para redefinir sua senha:\n" + link +
-                "\n\nEste link expira em 15 minutos.");
+        OkHttpClient client = new OkHttpClient();
+        MediaType mediaType = MediaType.parse("application/json");
+        String json = "{"
+                + "\"from\": {\"email\": \"vireyabrasil@gmail.com\"},"
+                + "\"to\": [{\"email\": \"" + to + "\"}],"
+                + "\"subject\": \"" + subject + "\","
+                + "\"text\": \"" + text + "\""
+                + "}";
 
-        mailSender.send(message);
+        RequestBody body = RequestBody.create(json, mediaType);
+        Request request = new Request.Builder()
+                .url("https://api.mailersend.com/v1/email")
+                .post(body)
+                .addHeader("Authorization", "Bearer " + apiKey)
+                .addHeader("Content-Type", "application/json")
+                .build();
+
     }
 }
